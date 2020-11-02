8f02cdc5d96d05e8bf3b03fcd23a543ee394638c
Isaac Owens <isaac.owens832@gmail.com>
2020-11-02 00:43:44 -0500

    Add tracker wrapper to Home component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
 import { Home } from '../components/Home';
 import { Tracker } from 'react-tracker';
-
+import { TrackerProvider } from 'react-tracker';
 const tracker = new Tracker();
 
+const pageViewListener = (event, trackingHistory) => {
+  window.dataLayer.push(event);
+};
+
+tracker.on('PAGE_VIEW', pageViewListener);
+
+const pageViewEvent = (pageId) => ({type: 'PAGE_VIEW', data: pageId});
+
 export default function Index() {
-  return <Home />;
+  return (
+    <TrackerProvider tracker={tracker}>
+      <Home />
+    </TrackerProvider>
+  );
 }
