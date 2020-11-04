b3c293e69fbb740335bb132abcae75c67f80ac8f
Isaac Owens <isaac.owens832@gmail.com>
2020-11-03 21:38:40 -0500

    Fix indentation on paywall html

--- components/Paywall.tsx
+++ components/Paywall.tsx
@@ -3,17 +3,18 @@ import React from 'react';
 export const Paywall = () => {
    return (
      <div className="modal">
-       <div className="overlay"></div>
-       <div className="modal_content">
-         <!-- Dynamic Section -->
-    <h2>Hey Awesome Modal!</h2>
-         <p>
-           Cupcake ipsum dolor sit amet pie brownie. Carrot cake wafer I love pie bear claw. Sweet cake cheesecake candy canes carrot cake marshmallow. Sweet roll I love sweet fruitcake donut chupa.
-    </p>
-    <button title="Close" className="close_modal">
-           <i className="fas fa-times"></i>
-         </button>
-       </div>
+      <div className="overlay"></div>
+      <div className="modal_content">
+
+        <h2>Hey Awesome Modal!</h2>
+        <p>
+          Cupcake ipsum dolor sit amet pie brownie. Carrot cake wafer I love pie bear claw. Sweet cake cheesecake candy canes carrot cake marshmallow. Sweet roll I love sweet fruitcake donut chupa.
+        </p>
+
+        <button title="Close" className="close_modal">
+          <i className="fas fa-times"></i>
+        </button>
+      </div>
      </div>
    );
 };
