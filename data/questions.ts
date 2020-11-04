7ff774e8682e1985340eb3f3d152b79519c893d8
Isaac Owens <isaac.owens832@gmail.com>
2020-11-03 22:42:30 -0500

    Add answers to questions

--- data/questions.ts
+++ data/questions.ts
@@ -2,20 +2,20 @@ import { OnePagerQuestion } from '../model/model';
 
 export const question1: OnePagerQuestion = {
   question: 'What holes do you have in your team and how do you plan to fill them?',
-  answer: '',
+  answer: 'We will be hiring people very soon!',
 }
 
 export const question2: OnePagerQuestion = {
   question: 'Why do you care about this?',
-  answer: '',
+  answer: 'Because reasons...',
 }
 
 export const question3: OnePagerQuestion = {
   question: 'Have you invested your own money? If not, why not?',
-  answer: '',
+  answer: 'Yes, our money, family\'s money, friend\'s money. You get the idea.'
 }
 
 export const question4: OnePagerQuestion = {
-  question: '',
-  answer: '',
+  question: 'What is your favorite color?',
+  answer: 'Green. Get it?  Like money.',
 }
